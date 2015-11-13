9dddf8dcd8c85958e7bbdd1b216b7c2cc50a7687
Tyler Darby <th3applek1d@tylers-macbook-pro.local>
2015-11-12 23:48:44 -0500

    Added launch screen and base login screen.

--- Fabric/ViewController.swift
+++ Fabric/ViewController.swift
@@ -10,16 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var splashImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func viewDidAppear(animated: Bool) {
+        // Move the icon away.
+        UIView.animateWithDuration(1, delay: 0.3, options: [.CurveEaseInOut], animations: {self.splashImage.center.y -= self.view.bounds.height }, completion: {Bool in self.performSegueWithIdentifier("loginScreen", sender: self)})
+    }
+}
 
-
+class LoginScreen: UIViewController {
+    
+    func prepareForSegue(){
+    
+    }
 }
 
